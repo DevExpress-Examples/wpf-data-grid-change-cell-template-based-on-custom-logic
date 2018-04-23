1af4972893745e4fe8413caa0f21cf11ef03f5d4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/EditorsDesignTime/Window1.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Collections;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Editors.Settings;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Core;
+using System.ComponentModel;
+
+namespace EditorsDesignTime {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            List<TestData> list = new List<TestData>();
+            for (int i = 0; i < 100; i++) {
+                list.Add(new TestData() { Editor = GetEditorType(i), Value = GetEditValue(i) });
+			}
+            grid.DataSource = list;
+        }
+        string GetEditorType(int index) { 
+            switch(index % 6) {
+                case 0:
+                    return "booleanEditor";
+                case 1:
+                    return "buttonEditor";
+                case 2:
+                    return "comboboxEditor";
+                case 3:
+                    return "dateEditor";
+                case 4:
+                    return "textEditor";
+                default:
+                    return string.Empty;
+            }
+        }
+        object GetEditValue(int index) {
+            switch(index % 6) {
+                case 0:
+                    return index % 2 == 0;
+                case 1:
+                    return "button edit " + index;
+                case 2:
+                    return index % 2 == 0 ? Alignment.Far : Alignment.Near;
+                case 3:
+                    return DateTime.Today.AddDays(index);
+                case 4:
+                    return "text " + index;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+    public class TestData {
+        public string Editor { get; set; }
+        public object Value { get; set; }
+    }
+    public class EditorTemplateSelector : DataTemplateSelector {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            GridCellData data = (GridCellData)item;
+            PropertyDescriptor property = TypeDescriptor.GetProperties(data.Data)["Editor"];
+            if(property == null)
+                return null;
+            string editorType = TypeDescriptor.GetProperties(data.Data)["Editor"].GetValue(data.Data) as string;
+            return string.IsNullOrEmpty(editorType) ? null : (DataTemplate)((FrameworkElement)container).FindResource(editorType);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to change a cell template based on some condition
+
+
+<p>This sample illustrates how to change a cell template based on some condition using <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfGridColumnBase_CellTemplateSelectortopic">CellTemplateSelector</a>. The main idea is to create a DataTemplateSelector descendant, and implement your own DataTemplates.</p>
+
+<br/>
+
+
